c39cbc272631bd15ec2d10c5f681d4492076c7d5
Davis Wojnovich <dwojnovich@gmail.com>
2020-03-08 23:35:34 -0400

    Updates

---
+++ precache-manifest.a6244c25ebd31224c1381a79f419a4e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe406e38e3155ac3b4a5d449f0c73405",
+    "url": "/c1-yelp/index.html"
+  },
+  {
+    "revision": "cff6f1e61d1d409c0990",
+    "url": "/c1-yelp/static/css/2.6adc84a7.chunk.css"
+  },
+  {
+    "revision": "9ed88df767bdd0574bbc",
+    "url": "/c1-yelp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cff6f1e61d1d409c0990",
+    "url": "/c1-yelp/static/js/2.c65383ce.chunk.js"
+  },
+  {
+    "revision": "959150e688866e3de05cb62dc4dcd7db",
+    "url": "/c1-yelp/static/js/2.c65383ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed88df767bdd0574bbc",
+    "url": "/c1-yelp/static/js/main.b8655ae3.chunk.js"
+  },
+  {
+    "revision": "3c95c16cff475a850580",
+    "url": "/c1-yelp/static/js/runtime-main.716da44f.js"
+  },
+  {
+    "revision": "fb499c441d61db6e452a600067885058",
+    "url": "/c1-yelp/static/media/drinkfood.fb499c44.png"
+  },
+  {
+    "revision": "882da0891e7283999f18a6efed988e1a",
+    "url": "/c1-yelp/static/media/headersnag.882da089.png"
+  },
+  {
+    "revision": "64f2316150fd6ad24826dd32471e8231",
+    "url": "/c1-yelp/static/media/moneyicon.64f23161.png"
+  },
+  {
+    "revision": "630d96bbb40088587c50e1fc7307c10a",
+    "url": "/c1-yelp/static/media/sitelogo.630d96bb.png"
+  }
+]);
